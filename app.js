0f748a94a376d34e1d9ea07adf9002e8cb50ab79
ebukanathan <ebukanathan@gmail.com>
2021-06-13 14:51:23 +0100

    'first-route-built'

---
+++ app.js
@@ -0,0 +1,21 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+
+// Routes
+
+app.get('/',(req,res)=>{
+    res.send('hello everybody')
+
+})
+
+
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+});
